983e18c4c89e0ff0e14895145c9a16d5b81853df
Iryna Hurova <iryna.gurova@gmail.com>
2024-05-23 21:22:17 +0300

    updated UI

--- GUI.py
+++ GUI.py
@@ -14,24 +14,19 @@ class GUI:
         self.map_size = map_size
 
         self.root = tk.Tk()
-        self.game_map = np.zeros((map_size*70, map_size*70, 3), np.uint8)+255
-        self.card = cv2.cvtColor(cv2.imread(os.path.join(SCRIPT_PATH, f"images/initial_card.png")), cv2.COLOR_BGR2RGB)
-        card_coords = (
-            map_size//2*70,
-            map_size//2*70
-        )
-        self.game_map[card_coords[0]:card_coords[0]+70, card_coords[1]:card_coords[1]+70, :] = self.card
-        self.game_map_with_options = self.game_map.copy()
+
+        self.card = np.zeros((70, 70, 3), np.uint8)
 
         self.map = Map()
-        self.map.update_map_dict(self.card, card_coords)
+        self.game_map_with_options = self.map.image.copy()
+        self.options = list()
 
         self.root.title("Carcessonne")
         self.root.geometry(f"{self.map_size*70+200}x{self.map_size*70+20}+100+100")
         self.root.resizable(width=True, height=True)
         
-        game_map  =  ImageTk.PhotoImage(image=Image.fromarray(self.game_map))
-        card  =  ImageTk.PhotoImage(image=Image.fromarray(self.card))
+        game_map = ImageTk.PhotoImage(image=Image.fromarray(self.map.image))
+        card = ImageTk.PhotoImage(image=Image.fromarray(self.card))
 
         tk.Label(self.root, image=game_map).place(x=10, y=10)
         tk.Button(self.root, text="Get Card", command=self.get_card).place(x=self.map_size*70+50, y=30)
@@ -39,6 +34,10 @@ class GUI:
         tk.Label(self.root, image=card).place(x=self.map_size*70+50, y=80)
         tk.Button(self.root, text="Turn Card", command=self.turn_card).place(x=self.map_size*70+50, y=160)
         tk.Button(self.root, text="Display Options", command=self.display_options).place(x=self.map_size*70+50, y=190)
+        tk.Label(self.root, text="Select Card Place").place(x=self.map_size*70+50, y=230)
+        self.input_box = tk.Entry(self.root, width=10)
+        self.input_box.place(x=self.map_size*70+50, y=250)
+        tk.Button(self.root, text="Place Card", command=self.place_card).place(x=self.map_size*70+50, y=280)
 
         self.root.mainloop()
 
@@ -51,9 +50,19 @@ class GUI:
         self.update_gui()
 
     def display_options(self):
-        options = self.map.find_available_option(self.card)
-        for number, location in enumerate(options):
+        self.options = self.map.find_available_option(self.card)
+        for number, location in enumerate(self.options):
             self.game_map_with_options = cv2.putText(self.game_map_with_options, text=str(number+1), org=(location[0]+35, location[1]+35), color=(0, 0, 0), thickness=2, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5)
+        self.update_gui()
+    
+    def place_card(self):
+        user_input = self.input_box.get()
+        if user_input.isdigit():
+            number = int(user_input)
+            if number<=len(self.options) and number>0:
+                self.map.update_map(self.card, self.options[number-1])
+                self.game_map_with_options = self.map.image.copy()
+                self.card = np.zeros((70, 70, 3), np.uint8)
         self.update_gui()
 
     def update_gui(self):
@@ -63,7 +72,7 @@ class GUI:
         tk.Label(self.root, image=game_map).place(x=10, y=10)
         tk.Label(self.root, image=card).place(x=self.map_size*70+50, y=80)
 
-        self.game_map_with_options = self.game_map.copy()
+        self.game_map_with_options = self.map.image.copy()
 
         self.root.mainloop()
 
--- Map.py
+++ Map.py
@@ -1,21 +1,33 @@
 import cv2
 import numpy as np
 from Card import Card
+import os
+
+SCRIPT_PATH = os.path.dirname(__file__)
 
 class Map:
-    def __init__(self) -> None:
+    def __init__(self, map_size=11) -> None:
+        self.map_size = map_size
         self.map = self.create_map()
+        self.image = np.zeros((map_size*70, map_size*70, 3), np.uint8)+255
+        card = cv2.cvtColor(cv2.imread(os.path.join(SCRIPT_PATH, f"images/initial_card.png")), cv2.COLOR_BGR2RGB)
+        card_coords = (
+            map_size//2*70,
+            map_size//2*70
+        )
+        self.image[card_coords[0]:card_coords[0]+70, card_coords[1]:card_coords[1]+70, :] = card
+        self.update_map(card, card_coords)
         
-    def create_map(self, map_size=11):
+    def create_map(self):
         map_list = list()
-        for x in range(map_size):
-            for y in range(map_size):
+        for x in range(self.map_size):
+            for y in range(self.map_size):
                 card = Card()
                 card.location = (x*70, y*70)
                 map_list.append(card)
         return  map_list
 
-    def update_map_dict(self, image, location):
+    def update_map(self, image, location):
         card = Card(image)
         card.location = location
         for place_number, place in enumerate(self.map):
@@ -29,6 +41,8 @@ class Map:
                 self.map[place_number].features["LEFT"] = card.features["RIGHT"]
             elif place.location == (location[0]-70, location[1]):
                 self.map[place_number].features["RIGHT"] = card.features["LEFT"]
+        
+        self.image[location[1]:location[1]+70, location[0]:location[0]+70, :] = image
 
     def find_available_option(self, image):
         card = Card(image)
